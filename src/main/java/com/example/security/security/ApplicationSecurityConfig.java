c8e79831ea3c5331bce5935fde31874d48ab9ec4
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-06-17 16:16:30 +0200

    basic Auth + created a User with role of STUDENT and another with a role of ADMIN with encoded passwords

---
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.example.security.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final PasswordEncoder passwordEncoder;
+
+  @Autowired
+  public ApplicationSecurityConfig(
+      PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+//    super.configure(http);
+    http
+        .authorizeRequests()
+        .antMatchers("/","index","/css/*","/js/*")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+  }
+
+  @Override
+  @Bean
+  protected UserDetailsService userDetailsService() {
+    UserDetails manalRzzlUser = User.builder()
+        .username("manalrzzl")
+        .password(passwordEncoder.encode("password"))
+        .roles("STUDENT") //internally will be ROLE_STUDENT
+        .build();
+
+    UserDetails ayaUser = User.builder()
+        .username("aya")
+        .password(passwordEncoder.encode("pass123"))
+        .roles("ADMIN")
+        .build();
+
+
+    return new InMemoryUserDetailsManager(
+        manalRzzlUser,
+        ayaUser
+    );
+//    return super.userDetailsService();
+  }
+}
