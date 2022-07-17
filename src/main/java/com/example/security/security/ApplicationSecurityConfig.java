9e23cdfca15a91104aa778d65e945da95be38d78
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-07-17 20:12:06 +0200

    disabling CSRF + implementing permission based authentication using antMatchers

--- src/main/java/com/example/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -5,6 +5,7 @@ import static com.example.security.security.ApplicationUserRole.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -31,11 +32,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
 //    super.configure(http);
     http
+        .csrf().disable()
         .authorizeRequests()
         .antMatchers("/","index","/css/*","/js/*")
         .permitAll()
         //this will protect the api to only be accessed by students
         .antMatchers("/api/**").hasRole(STUDENT.name())
+        .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+        .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+        .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+        .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
         .anyRequest()
         .authenticated()
         .and()
@@ -58,7 +64,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         .build();
 
     UserDetails tomUser = User.builder()
-        .username("aya")
+        .username("tom")
         .password(passwordEncoder.encode("pass123"))
         .roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
         .build();
